db49e5b6aeb1c8b2bdf71992c7bcdefa0103f86b
Mark McGranaghan <mmcgrana@gmail.com>
2008-12-13 00:08:37 -0500

    these will be usefull

---
+++ cwsg-sandbox/serve.clj
@@ -0,0 +1,13 @@
+(require 'cwsg.core
+         'cwsg.middleware.file-content-info
+         'cwsg.middleware.string-content-length
+         'cwsg.middleware.static
+         'cwsg.apps.dump)
+
+(import '(java.io File))
+
+(cwsg.core/serve {:port 8000}
+  (cwsg.middleware.file-content-info/wrap
+    (cwsg.middleware.string-content-length/wrap
+      (cwsg.middleware.static/wrap (File. "public")
+        cwsg.apps.dump/app))))
