f8b6c31b3b5c8789566ca2e148c38a0579ca315c
Mark McGranaghan <mmcgrana@gmail.com>
2009-01-07 13:54:51 -0500

    cljre now at github.com/mmcgrana/cljre.com, in prep for softlaunch

--- cljre/bin/serve.clj
+++
@@ -1,5 +0,0 @@
-(require 'cwsg.handlers.jetty 'cljre.app)
-
-(let [env (if (= "prod" (first *command-line-args*)) :prod :dev)]
-  (cwsg.handlers.jetty/run {:port 8080}
-    (cljre.app/build-app env)))
