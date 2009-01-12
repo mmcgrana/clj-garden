41bc565d261879a03a729e6dd3bf7f75cd8c1750
Mark McGranaghan <mmcgrana@gmail.com>
2009-01-12 12:55:06 -0500

    Weld and Ring now have their own projects at http://github.com/mmcgrana/{weld,ring}

--- ring/test/run.clj
+++
@@ -1,9 +0,0 @@
-(use 'clj-unit.core)
-(require-and-run-tests
-  'ring.builder-test
-  'ring.dump-test
-  'ring.lint-test
-  'ring.file-test
-  'ring.file-info-test
-  'ring.reloading-test
-  'ring.show-exceptions-test)
--- weld/test/run.clj
+++
@@ -1,8 +0,0 @@
-(use 'clj-unit.core)
-(require-and-run-tests
-  'weld.app-test
-  'weld.controller-test
-  'weld.http-utils-test
-  'weld.request-test
-  'weld.routing-test
-  'weld.utils-test)
